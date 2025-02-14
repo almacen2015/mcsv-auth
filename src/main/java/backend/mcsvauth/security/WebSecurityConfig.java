ba227b5df9f935fa45579c906a00e200dd8bfa65
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-02-13 19:06:09 -0500

    se agregaron dependencias jpa

---
+++ src/main/java/backend/mcsvauth/security/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package backend.mcsvauth.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+
+}
