8f5a3f94f094d5905ca7306b00d372da0418593a
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-02-15 17:28:31 -0500

    se agrego clase para validar el token y se agrego filter antes de la autenticacion basica en securityconfig

--- src/main/java/backend/mcsvauth/security/WebSecurityConfig.java
+++ src/main/java/backend/mcsvauth/security/WebSecurityConfig.java
@@ -1,6 +1,9 @@
 package backend.mcsvauth.security;
 
+import backend.mcsvauth.filter.JwtTokenValidator;
 import backend.mcsvauth.service.impl.UsuarioServiceImpl;
+import backend.mcsvauth.util.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,17 +12,20 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,7 +35,9 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
 
-        return http.build();
+        return http
+                .addFilterBefore(new JwtTokenValidator(jwtUtils), BasicAuthenticationFilter.class) //se agrega el filtro que valida el token antes del filtro de autenticacion basicas
+                .build();
     }
 
     @Bean
